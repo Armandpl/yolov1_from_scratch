5d93f569342b638c0f7f105a58d6a929355a6376
Armandpl <adpl33@gmail.com>
2021-11-18 19:57:29 +0000

    create yolo network based on pretrained feature extractors from torchvision

---
+++ network.py
@@ -0,0 +1,22 @@
+import torch
+import torchvision.models as models
+
+
+class Yolo(torch.nn.Module):
+    def __init__(self, feature_extractor="resnet50", S=7,
+                 classes=20, pretrained=True):
+        super(Yolo, self).__init__()
+        self.S = S
+        self.classes = classes
+        self.feature_extractor = \
+            models.__dict__[feature_extractor](pretrained=pretrained)
+        self.output_layer = torch.nn.Linear(
+            self.feature_extractor.fc.in_features, S*S*(classes+5))
+
+        self.feature_extractor.fc = torch.nn.Identity()
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = self.output_layer(x)
+        x = x.reshape((self.S, self.S, self.classes+5))
+        return x
