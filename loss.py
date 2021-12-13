e98e9210397235180220a77fb98cd4a1e39f85dd
Armandpl <adpl33@gmail.com>
2021-12-13 18:34:29 +0000

    refactor loss function

--- loss.py
+++ loss.py
@@ -1,52 +1,29 @@
 import torch
+from torchvision import ops
 
 
-def xywh2xyxy(xywh):
-    x = xywh[..., 0]
-    y = xywh[..., 1]
-    w = xywh[..., 2]
-    h = xywh[..., 3]
+def batch_iou(pred_boxes, target_boxes):
+    """
+    takes in a batches of boxes (N, S, S, 4) in cx,cy,w,h format
+    outputs ious (N, S, S)
+    """
+    bs = pred_boxes.size()[0]
+    S = pred_boxes.size()[1]
 
-    x1, y1 = x - w/2, y - h/2
-    x2, y2 = x + w/2, y + h/2
+    # flatten boxes bc torchvision ops takes list of boxes
+    target_boxes = target_boxes.reshape((-1, S**2, 4))
+    pred_boxes = pred_boxes.reshape((-1, S**2, 4))
 
-    xyxy = torch.stack((x1, y1, x2, y2), dim=3)
+    # convert boxes
+    target_boxes = ops.box_convert(target_boxes, in_fmt="cxcywh", out_fmt="xyxy")
+    pred_boxes = ops.box_convert(pred_boxes, in_fmt="cxcywh", out_fmt="xyxy")
 
-    return xyxy
+    # compute ious
+    ious = torch.zeros((bs, S**2), device=pred_boxes.device)
+    for b_idx in range(bs):
+        ious[b_idx] = ops.box_iou(target_boxes[b_idx], pred_boxes[b_idx]).diag()
 
-# def xyxy2xywh(xyxy):
-
-# topxtopywh2xywh(topxtopywh):
-
-
-def area(x1, y1, x2, y2):
-    L = (x2 - x1).clamp(min=0)  # if they don't intersect
-    w = (y2 - y1).clamp(min=0)
-    return w*L
-
-
-def IoU(box1, box2):
-    box1 = xywh2xyxy(box1)
-    box2 = xywh2xyxy(box2)
-
-    # find intersection coords
-    x1 = torch.max(box1[..., 0], box2[..., 0])
-    y1 = torch.max(box1[..., 1], box2[..., 1])
-    x2 = torch.min(box1[..., 2], box2[..., 2])
-    y2 = torch.min(box1[..., 3], box2[..., 3])
-
-    # compute area
-    intersection = area(x1, y1, x2, y2)
-
-    # find union coords
-    x1 = torch.min(box1[..., 0], box2[..., 0])
-    y1 = torch.min(box1[..., 1], box2[..., 1])
-    x2 = torch.max(box1[..., 2], box2[..., 2])
-    y2 = torch.max(box1[..., 3], box2[..., 3])
-    union = area(x1, y1, x2, y2)
-
-    return torch.nan_to_num(intersection/union, nan=0.0)
-
+    return ious
 
 class YoloLoss(torch.nn.Module):
     def __init__(self):
@@ -56,74 +33,60 @@ class YoloLoss(torch.nn.Module):
         self.S = 7
         self.B = 2
         self.n_classes = 80
-        self.loss = torch.nn.MSELoss(reduction='none')
+        self.mse = torch.nn.MSELoss(reduction='sum')
 
     def forward(self, predictions, target):
         predictions =\
-            predictions.reshape(-1, self.S, self.S, self.n_classes + self.B*5)
+            predictions.reshape(-1, self.S, self.S, self.n_classes + 5*self.B)
 
-        ious_b1 = IoU(predictions[:, :, :, -4:], target[:, :, :, -4:])
-        ious_b2 = IoU(predictions[:, :, :, -10:-5], target[:, :, :, -4:])
-        ious = torch.cat([ious_b1.unsqueeze(0), ious_b2.unsqueeze(0)], dim=0)
-        ious_max, ious_idx = torch.max(ious, dim=0)
+        target_boxes = target[..., -4:]
+        pred_boxes_1 = predictions[..., -4:]
+        pred_boxes_2 = predictions[..., -9:-5]
 
-        # is there an object in the cell
-        obj = target[:, :, :, -5]
+        # ious mask
+        ious_b1, ious_b2 = batch_iou(pred_boxes_1, target_boxes),\
+                           batch_iou(pred_boxes_2, target_boxes)
+
+        ious = torch.stack([ious_b1, ious_b2], dim=0)
+        _, ious_idx = torch.max(ious, dim=0)
+        ious_idx = ious_idx.reshape((-1, self.S, self.S))
+
+        # (N, S, S)
+        ious_mask_box_1_ = torch.ones_like(ious_idx) - ious_idx
+        ious_mask_box_2_ = ious_idx
+        
+        # obj mask: is there an object in the cell
+        # (N, S, S)
+        obj_mask_ = target[..., -5]
 
         # TODO this only works for B = 2
 
         # coords loss
-        ious_idx_box_1 = torch.ones_like(ious_idx) - ious_idx
-        ious_idx_box_2 = ious_idx
+        obj_mask = obj_mask_.unsqueeze(3).expand(-1, -1, -1, 4)
+        ious_mask_box_1 = ious_mask_box_1_.unsqueeze(3).expand(-1, -1, -1, 4)
+        ious_mask_box_2 = ious_mask_box_2_.unsqueeze(3).expand(-1, -1, -1, 4)
 
-        coords_loss_box_1 =\
-            self.loss(predictions[..., -4:-2], target[..., -4:-2]) +\
-            torch.nan_to_num(
-                self.loss(torch.sqrt(predictions[..., -2:]),
-                          torch.sqrt(target[..., -2:])),
-                nan=0.0
-            )
+        # select box w/ biggest iou when there is object (else 0)
+        pred_boxes = obj_mask * (ious_mask_box_1 * pred_boxes_1 + ious_mask_box_2 * pred_boxes_2) 
 
-        coords_loss_box_1 = torch.sum(coords_loss_box_1, dim=-1) *\
-            ious_idx_box_1
+        dimensions = torch.sign(pred_boxes[..., -2:]) * torch.sqrt(
+            torch.abs(pred_boxes[..., -2:]) + 1e-6  # numerical stability
+        )
+        target_dimensions = torch.sqrt(target_boxes[..., -2:])
 
-        coords_loss_box_2 =\
-            self.loss(predictions[..., -9:-7], target[..., -4:-2]) +\
-            torch.nan_to_num(
-                self.loss(torch.sqrt(predictions[..., -7:-5]),
-                          torch.sqrt(target[..., -2:])),
-                nan=0.0
-            )
+        center_loss = self.mse(pred_boxes[..., :-3], target_boxes[..., :-3]) * self.lambda_coords
+        dimensions_loss = self.mse(dimensions, target_dimensions) * self.lambda_coords
+        coords_loss = center_loss + dimensions_loss
 
-        coords_loss_box_2 = torch.sum(coords_loss_box_2, dim=-1) *\
-            ious_idx_box_2
+        # obj/noobj loss
+        obj_pred = (predictions[..., -5]*ious_mask_box_1_ + predictions[..., -10]*ious_mask_box_2_)
+        obj_loss = self.mse(obj_mask_ * obj_pred, target[..., -5])
 
-        coords_loss = self.lambda_coords * obj *\
-            (coords_loss_box_1 + coords_loss_box_2)
-        coords_loss = torch.sum(coords_loss, (1, 2))
+        noobj_mask = torch.ones_like(obj_mask_) - obj_mask_
+        noobj_loss = self.mse(noobj_mask * obj_pred, target[..., -5]) * self.lambda_noobj
 
-        # obj loss
-        obj_loss_b1 = self.loss(predictions[..., -5], target[..., -5]) *\
-            ious_idx_box_1
+        # classes loss
+        obj_mask = obj_mask_.unsqueeze(3).expand(-1, -1, -1, self.n_classes)
+        classes_loss = self.mse(predictions[..., :-10]*obj_mask, target[..., :-5])
 
-        obj_loss_b2 = self.loss(predictions[..., -10], target[..., -5]) *\
-            ious_idx_box_2
-
-        obj_loss_raw = obj_loss_b1 + obj_loss_b2
-        obj_loss = torch.sum(obj_loss_raw * obj, dim=(1, 2))
-
-        # no obj loss
-        noobj = torch.ones_like(obj) - obj
-        noobj_loss = torch.sum(obj_loss_raw * noobj, dim=(1, 2)) *\
-            self.lambda_noobj
-
-        classes_loss =\
-            torch.sum(
-                self.loss(predictions[:, :, :, :-10], target[:, :, :, :-5]),
-                dim=-1
-            ) * obj
-        classes_loss = torch.sum(classes_loss, dim=(1, 2))
-
-        loss = coords_loss + obj_loss + noobj_loss + classes_loss
-        # print(coords_loss, obj_loss, noobj_loss, classes_loss)
-        return torch.sum(loss)
+        return coords_loss, obj_loss, noobj_loss, classes_loss
